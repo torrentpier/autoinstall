73c67756988bc94b0761ed167c08b8a3ecf63276
Sergei Solovev <31823239+SeAnSolovev@users.noreply.github.com>
2024-11-23 23:40:48 +0300

    Simplified logic, fixed errors, edited syntax (#3)

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@
 # Arrays and variables used
 suppOs=("debian" "ubuntu")
 aptOs=("debian" "ubuntu")
-currOs=$(cat /etc/os-release | grep ^ID | awk -F= '{print $2}')
-logsInst="$(dirname "$0")/torrentpier_install.log"
+currOs=$(grep ^ID /etc/os-release | awk -F= '{print $2}')
+logsInst="/var/log/torrentpier_install.log"
 
 # User verification
 if [ "$(whoami)" != "root" ]; then
@@ -33,23 +33,24 @@ done
 
 # Downloading and running the installation file
 if $foundOs; then
-    if [[ " ${currOs} " =~ " ${aptOs} " ]]; then
-        apt-get -y update 2>&1 | tee -a "$logsInst"
-        apt-get -y dist-upgrade 2>&1 | tee -a "$logsInst"
+    for os in "${aptOs[@]}"; do
+        if [[ "$os" == "$currOs" ]]; then
+            apt-get -y update 2>&1 | tee -a "$logsInst"
+            apt-get -y dist-upgrade 2>&1 | tee -a "$logsInst"
 
-        if [ -f "apt.install.sh" ]; then
-            apt-get install -y sudo 2>&1 | tee -a "$logsInst"
-            sudo chmod +x apt.install.sh 2>&1 | sudo tee -a "$logsInst"
-            sudo ./apt.install.sh 2>&1 | sudo tee -a "$logsInst"
-        else
-            apt-get install -y sudo jq curl 2>&1 | tee -a "$logsInst"
+            apt-get install -y sudo jq curl zip unzip 2>&1 | tee -a "$logsInst"
+
             sudo mkdir -p /tmp/torrentpier 2>&1 | sudo tee -a "$logsInst"
-            curl -s https://api.github.com/repos/SeAnSolovev/torrentpier-autoinstall | jq -r 'map(select(.prerelease == true)) | .[0].zipball_url' | xargs -n 1 curl -L -o /tmp/torrentpier/autoinstall.zip 2>&1 | sudo tee -a "$logsInst"
-            sudo unzip -o /tmp/enginegp/enginegp.zip -d /tmp/enginegp/autoinstall 2>&1 | sudo tee -a "$logsInst"
-            sudo chmod +x /tmp/enginegp/autoinstall/apt.install.sh 2>&1 | sudo tee -a "$logsInst"
-            sudo /tmp/enginegp/autoinstall/apt.install.sh 2>&1 | sudo tee -a "$logsInst"
+            sudo rm -rf /tmp/torrentpier/* 2>&1 | sudo tee -a "$logsInst"
+
+            curl -s https://api.github.com/repos/SeAnSolovev/torrentpier-autoinstall/releases | jq -r 'map(select(.prerelease == true)) | .[0].zipball_url' | xargs -n 1 curl -L -o /tmp/torrentpier/autoinstall.zip 2>&1 | sudo tee -a "$logsInst"
+            sudo unzip -o /tmp/torrentpier/autoinstall.zip -d /tmp/torrentpier 2>&1 | sudo tee -a "$logsInst"
+            sudo mv /tmp/torrentpier/*autoinstall-* /tmp/torrentpier/autoinstall 2>&1 | sudo tee -a "$logsInst"
+
+            sudo chmod +x /tmp/torrentpier/autoinstall/apt.install.sh 2>&1 | sudo tee -a "$logsInst"
+            sudo /tmp/torrentpier/autoinstall/apt.install.sh 2>&1 | sudo tee -a "$logsInst"
         fi
-    fi
+    done
 else
     echo "Your system is not supported." 2>&1 | tee -a "$logsInst"
 fi
